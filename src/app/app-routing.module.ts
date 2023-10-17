0902498b931d54c7dcdbdd88b49e7ab4e4523eac
Mellowbun <couch.devan@gmail.com>
2023-10-17 10:52:18 -0700

    added to app.compon html and css, create navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ItemComponent } from './item/item.component';
 
-
 const routes: Routes = [
-  { path: "/item", component: ItemComponent },
-  { path: "**" , pathMatch: 'full', component: HomeComponent }
+  { path: "item", component: ItemComponent },
+  { path: "item/:item", component: ItemComponent },
+  { path: "**" , pathMatch: 'full', component: HomeComponent },
 ];
 
 @NgModule({
