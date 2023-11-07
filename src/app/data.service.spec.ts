e7b24d3394eb5e857bf52edffca3086bfb3c64f9
Mellowbun <couch.devan@gmail.com>
2023-11-07 10:11:21 -0800

    fixed an issue with data.sevices.spec.ts

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,35 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { TestBed } from '@angular/core/testing';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ItemComponent } from './item/item.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CardItemComponent } from './card/card-item/card-item.component';
-import { CardListComponent } from './card/card-list/card-list.component';
 import { DataService } from './data.service';
-import { HttpClientModule } from '@angular/common/http';
-import { GameDetailComponent } from './game-detail/game-detail.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ItemComponent,
-    HomeComponent,
-    NavbarComponent,
-    CardItemComponent,
-    CardListComponent,
-    GameDetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [
-    DataService
-  ],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+describe('DataService', () => {
+  let service: DataService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(DataService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
